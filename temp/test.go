4d2037258a999594dac97f4d25b073ce56cecbba
viacheslav.demianov <viacheslav.demianov@thinksurance.de>
2024-10-02 17:55:48 +0200

    roadmap

--- temp/test.go
+++ temp/test.go
@@ -3,6 +3,7 @@ package main
 import "fmt"
 
 func main() {
+	// Slice
 	var myArr [3]int
 
 	var mySlice []int
@@ -15,4 +16,12 @@ func main() {
 	fmt.Printf("Sl2 equal %#v\n", mySlice2 == nil)
 	fmt.Printf("Sl3 equal %#v\n", mySlice3 == nil)
 
+	// Map
+	//m := make(map[string]int)
+	var m map[string]int
+
+	//m["key"] = 42
+	//fmt.Printf("Map %#v\n", m)
+
+	fmt.Printf("m equal nil %#v\n", m == nil)
 }
